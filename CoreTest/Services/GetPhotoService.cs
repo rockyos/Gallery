988a28ef4cfcaccbcc462bde3c963caeef28d164
rockyos <46592602+rockyos@users.noreply.github.com>
2019-03-12 16:19:40 +0200

    Finally fix problems with update page in GetPhotoService

--- CoreTest/Services/GetPhotoService.cs
+++ CoreTest/Services/GetPhotoService.cs
@@ -24,26 +24,21 @@ namespace CoreTest.Services
         public async Task<List<PhotoDTO>> GetPhotoDBandSessionAsync( ISession Session, string sessionkey)
         {
             List<Photo> photosFromSession = Session.Get<List<Photo>>(sessionkey);
-            if (photosFromSession != null)
-            {
-                photosFromSession = photosFromSession.GroupBy(x => x.Guid).Select(y => y.First()).ToList();
-            }
-
             List<Photo> photoFromDB = await (await UnitOfWork.PhotoRepository.GetAllAsync()).ToListAsync();
             if (photosFromSession != null)
             {
-                List<Photo> equalPhoto = new List<Photo>(); // find dublicate Photo in session and DB 
+                List<Photo> hidePhotoFromSession = new List<Photo>();
                 foreach (var item in photosFromSession)
                 {
                     Photo photo = photoFromDB.Find(c => c.Guid == item.Guid);
                     if (photo != null)
                     {
-                        equalPhoto.Add(photo);
+                        photoFromDB.Remove(photo);
+                        hidePhotoFromSession.Add(item);
                     }
                 }
-                photoFromDB.RemoveAll(i => equalPhoto.Contains(i));
+                photosFromSession.RemoveAll(i => hidePhotoFromSession.Contains(i));
                 photoFromDB.AddRange(photosFromSession);
-               
             }
             List<PhotoDTO> photosDTO = _mapper.Map<List<Photo>, List<PhotoDTO>>(photoFromDB);
             return photosDTO;
