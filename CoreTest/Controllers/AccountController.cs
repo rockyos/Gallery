9278d2e86ff81859c152e97f822ece223df73818
serhiy.krasovskyy <serhiy.krasovskyy@onlini.co>
2019-04-11 14:42:08 +0300

    Fixed callback URL.

--- CoreTest/Controllers/AccountController.cs
+++ CoreTest/Controllers/AccountController.cs
@@ -56,11 +56,8 @@ namespace CoreTest.Controllers
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                    var callbackUrl = Url.Page(
-                        "/Account/ConfirmEmail",
-                        pageHandler: null,
-                        values: new { userId = user.Id, code = code },
-                        protocol: Request.Scheme);
+                    var callbackUrl = Url.Action("ConfirmEmail", "Account", new {userId = user.Id, code = code});
+                    callbackUrl = $"http://localhost:49508{callbackUrl}";
 
                     await _emailSender.SendEmailAsync(model.Email, "Confirm your email",
                       $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
